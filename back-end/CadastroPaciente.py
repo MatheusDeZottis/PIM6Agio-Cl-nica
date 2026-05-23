5ad2d8199af6092e943a1d9e84d0a6866e2165a2
jef09ac <jef09ac@gmail.com>
2026-05-23 18:46:19 -0300

    Cadastro de Pacientes
    Programa que captura os dados do paciente.

---
+++ back-end/CadastroPaciente.py
@@ -0,0 +1,6 @@
+# Solicitar informações do usuário
+nome_paciente = input ("Digite o nome do paciente: ")
+cpf_paciente = input ("Digite o CPF do paciente: ")
+data_nascimento_paciente = input ("Digite a data de nascimento do paciente: ")
+tefefone_paciente = input ("Digite o telefone do paciente: ")
+
