050fd4de016314dc66fb336cf703ff9d8152f371
jef09ac <jef09ac@gmail.com>
2026-05-23 21:32:16 -0300

    Cadastro de pacientes
    Programa para cadastrar os pacientes.

--- back-end/CadastroPaciente.py
+++ back-end/CadastroPaciente.py
@@ -3,4 +3,3 @@ nome_paciente = input ("Digite o nome do paciente: ")
 cpf_paciente = input ("Digite o CPF do paciente: ")
 data_nascimento_paciente = input ("Digite a data de nascimento do paciente: ")
 tefefone_paciente = input ("Digite o telefone do paciente: ")
-
